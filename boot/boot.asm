fa5a52a2f20858b45f49d8a57d065b5bd5114e2b
tingwei628 <sbvq6p199xe@gmail.com>
2021-12-30 18:53:12 +0800

    add comment to boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,7 +2,7 @@
 [ORG 0x7c00] ; start to run at 0x7c00
 
 ; initialization of segment registers and stack pointer 
-; now, ax is 0
+; now, ax=0
 start:
     xor ax,ax   
     mov ds,ax
@@ -11,13 +11,27 @@ start:
     mov sp,0x7c00
 
 PrintMessage:
+    ; function code, which is "Write string"
     mov ah,0x13
+    ; cursor at the end of string
     mov al,1
+    ; bh is page number
+    ; bl is color
+    ; bh is higher part of bx and bl is lower part of bx
+    ; set bl=0xa (0xa=bright green which is character color)
     mov bx,0xa
+    ; dx is string position
+    ; dh is row (higher part of dx)
+    ; dl is column (lower part of dx)
+    ; set dh=0 and dl=0
     xor dx,dx
+    ;ES:BP = Offset of string
     mov bp,Message
-    mov cx,MessageLen 
-    int 0x10 ; BIOS interrupt number to call BIOS service
+    ; cx is number of characters in string
+    mov cx,MessageLen
+    ; BIOS interrupt call
+    ; 0x10 = BIOS interrupt number
+    int 0x10
 
 End:
     hlt    
@@ -30,21 +44,35 @@ MessageLen: equ $-Message
 ; $-$$ is current section size (in bytes)
 times (0x1be-($-$$)) db 0
 
-; here is partition table entries (which is total 64 bytes)
-; there're 4 entries and only define the first entry
-    ; db 80h
+; here are partition table entries (which is total 64 bytes)
+; there are 4 entries and only define the first entry
+; each entry has 16 bytes (64/4=16)
+    ; this is the first partition entry format
+    ; boot indicator, 0x80=bootable partition
     db 0x80
+    ; starting CHS
+    ; C=cylinder, H=head, S=sector
+    ; the range for cylinder is 0 through 1023
+    ; the range for head is 0 through 255 inclusive
+    ; The range for sector is 1 through 63
+    ; address of the first sector in partition
+    ; 0(head),2(sector),0(cylinder)
     db 0,2,0
-    ; db 0f0h
+    ; partition type
     db 0xf0
-    ; db 0ffh,0ffh,0ffh
+    ; ending of CHS
+    ; address of last absolute sector in partition
     db 0xff, 0xff, 0xff
+    ; starting sector (LBA)
     dd 1
+    ; size (number of sectors in partition)
+    ; here is (20*16*63-1) * 512 bytes/ (1024 * 1024) = 10 mb
     dd (20*16*63-1)
 	
+    ; other 3 entries are set t0 0
     times (16*3) db 0
     
-    ; BIOS identifier
+    ; Boot Record signature
     ; here is the same as dw 0aa55h
     db 0x55
     db 0xaa
