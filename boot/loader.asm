ed31ef25a3639c5801b1f69c88ad7e7f69d9d915
tingwei628 <sbvq6p199xe@gmail.com>
2021-12-31 14:07:16 +0800

    get memory info message

--- boot/loader.asm
+++ boot/loader.asm
@@ -51,6 +51,42 @@ LoadKernel:
     int 0x13
     jc  ReadError
 
+; INT 15H = Miscellaneous system services
+; get memory map
+GetMemInfoStart:
+    ; note that the upper 16-bits of eax should be set to 0
+    ; function code, 0xe820
+    mov eax, 0xe820
+    ; set edx to the magic number 0x534D4150
+    ; smap
+    mov edx, 0x534d4150
+    ; The length of memory block in bytes 
+    mov ecx, 20
+    ; ES:DI
+    ; start address of memory block
+    mov edi, 0x9000
+    ; ebx = Continuation (the first call => ebx must be 0)
+    xor ebx, ebx
+    ; 0xe820 function that can detect memory areas above 4G
+    ; check if 0xe820 service is available
+    int 0x15
+    jc NotAvailable
+
+GetMemInfo:
+    ; move to next memory block (each memory block has 20 bytes)
+    add edi, 20
+    ; ebx = Continuation
+    ; ebx must be preserved and do not change ebx
+    mov eax, 0xe820
+    mov edx, 0x534d4150
+    mov ecx, 20
+    int 0x15
+    ; jump if there is NO next memory block
+    jc PrintMessage
+    ; test => ebx & ebx, if result!=0, ZF=0, then jump
+    test ebx, ebx
+    jnz GetMemInfo
+
 PrintMessage:
     mov ah, 0x13
     mov al, 1
@@ -67,6 +103,6 @@ End:
     jmp End
 
 DriveId:    db 0
-Message:    db "kernel is loaded"
+Message:    db "Get memory info done"
 MessageLen: equ $-Message
 ReadPacket: times 16 db 0
